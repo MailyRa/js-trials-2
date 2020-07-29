2892c9c6d703eb4275ee6db2eda1268fa273484e
Maily Ramos <maily.ramos1993@gmail.com>
2020-07-29 17:10:56 +0000

    function calculateTotal

--- objects.js
+++ objects.js
@@ -3,9 +3,19 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
-}
+    const wordCount = {};
 
+    for (const word of phrase.split('  ')) {
+        if (wordCount[word]) {
+            wordCount[word] += 1;
+        }
+        else {
+            wordCount[word] = 1;
+        }
+    }
+    return wordCount;
+};
+countWords(["I my name is"])
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
